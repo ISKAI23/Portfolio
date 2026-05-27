a3dd5c90a990d6fe3f4f270990dfe6d057dfe109
Bruno Riul <brunoc.riul@gmail.com>
2026-05-26 22:09:42 -0300

    Adicionado uma 'logo' no header (bugada) Adicionado ícone na index Primeiro Script: automaticamente importa o header e o footer para todas as páginas que contenham a importação do script! Header sem degradê - Sugestão do Cã

---
+++ assets/js/import_footer-header.js
@@ -0,0 +1,48 @@
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
+
+header.innerHTML = `
+        <div class="header_logo">
+            // <a href="https://www.youtube.com/@RapazeseTeclas" target="_blank" rel="noopener noreferrer"><img src="./assets/img/gengar.png" alt="Logo"></a>
+        </div>   
+
+    <div class="header_total">
+        <div class="header_inicio">
+                <a href="index.html"><h3>Início</h3></a>
+        </div>
+
+        <div class="header_quem_sou_eu">
+            <a href="quem_sou_eu.html"><h3>Quem sou eu?</h3></a>    
+        </div>
+
+        <div class="header_projetos">
+            <a href="projetos.html"><h3>Meus Projetos</h3></a> 
+        </div>
+
+        <div class="header_contatos">
+            <a href="#contato"><h3>Contatos</h3></a>
+        </div>
+    </div>
+`;
+
+footer.innerHTML = `
+    <div class="footer_pessoal">
+                <h2>Infos</h2>
+                <p>Portfólio pessoal com projetos, estudos e experimentações em desenvolvimento web, interface e tecnologia.</p>
+            </div>
+            <div class="footer_projetos">
+                <h2>Mais projetos</h2>
+                <p>Conheça mais dos meus projetos <a href="projetos.html">clicando aqui!</a></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste reiciendis, labore quam quia voluptates id modi ipsum impedit esse voluptatum quasi magni laborum odio vitae quos voluptatem qui fuga perspiciatis.</p>
+            </div>
+            <div class="footer_contato" id="contato">
+                <h2>Contatos</h2>
+                <p>Caso queira me contatar, aqui estão minhas redes sociais:</p>
+                <div class="contato_imagens">
+                    <address>
+                        <a href="https://www.instagram.com/_bruurb.rpz" target="blank" rel="noopener noreferrer"><img src="./assets/img/instagram-logo-facebook-2-svgrepo-com.svg" alt="@bruurb.rpz"></a>
+                        <a href="https://github.com/ISKAI23/ISKAI23.git" target="blank" rel="noopener noreferrer"><img src="./assets/img/github-142-svgrepo-com.svg" alt="GitHub ISKAI23"></a>
+                    </address>
+                </div>
+            </div>
+`;
